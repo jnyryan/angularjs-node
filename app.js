d6da066deb313df14a5393d9c8cbff85b741f427
John Ryan <jaryan@glgroup.com>
2014-02-17 15:37:14 +0000

    updated app

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
-var express = require('express');
-var fs = require("fs");
+// web.js
+var express = require("express");
+var logfmt = require("logfmt");
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
- 
+app.use(logfmt.requestLogger());
+
 app.get('/', function(req, res) {
-  res.sendfile('./public/views/index.html');
+    res.send('Hello World!');
 });
 
-app.get('/data', function(req, res) {
-  console.log("In rest api. returning data"); 
-  var file = fs.readFileSync("public/data/cast.json", "utf-8" );
-  res.json(JSON.parse(file)) ;
-}); 
- 
-app.listen(3000);
-console.log("Listening");
-
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+    console.log("Listening on " + port);
+});
